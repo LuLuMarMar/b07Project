0296157030f6847095957ed5190a5c0a60900180
Rhodwell <rjb.malicdem@gmail.com>
2023-11-29 11:48:42 -0500

    Added cases when submitting feedbacks

--- app/src/main/java/com/example/b07_project/FeedbackFragment.java
+++ app/src/main/java/com/example/b07_project/FeedbackFragment.java
@@ -3,6 +3,10 @@ package com.example.b07_project;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DatabaseReference;
@@ -17,12 +21,20 @@ public class FeedbackFragment extends AppCompatActivity {
         setContentView(R.layout.fragment_feedback);
         feedbackReference = FirebaseDatabase.getInstance().getReference("feedback");
         Button addFeedbackToDB = findViewById(R.id.feedback_button);
-        final TextInputEditText textInput = findViewById(R.id.comment_text_input);
+        RadioGroup ratingGroup = findViewById(R.id.ratingGroup);
+        TextInputEditText textInput = findViewById(R.id.comment_text_input);
         addFeedbackToDB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String comment = textInput.getText().toString();
-                Feedback feedback = new Feedback(comment, 0);
+                int rating = ratingGroup.getCheckedRadioButtonId();
+                if(rating != -1) {
+                    RadioButton ratingButton = findViewById(rating);
+                    rating = Integer.parseInt(ratingButton.getText().toString());
+                } else {
+                    rating = 0;
+                }
+                Feedback feedback = new Feedback(comment, rating);
                 feedbackReference.child("name_of_event").setValue(feedback);
             }
         });
