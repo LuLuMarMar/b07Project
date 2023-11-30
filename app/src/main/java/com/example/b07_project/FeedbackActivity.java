100ea10d1636b215c209746e27a55ffbb3f86548
anish422 <anish_shah0516@hotmail.com>
2023-11-30 14:36:16 -0500

    ,

--- app/src/main/java/com/example/b07_project/FeedbackActivity.java
+++ app/src/main/java/com/example/b07_project/FeedbackActivity.java
@@ -3,9 +3,11 @@ package com.example.b07_project;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FeedbackActivity extends AppCompatActivity {
@@ -22,18 +25,39 @@ public class FeedbackActivity extends AppCompatActivity {
     private DatabaseReference feedbackReference;
     private ListView listViewFeedback;
     private List<String> feedbackList;
+    private ArrayAdapter<String> feedbackAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
 
-        // Initialize Firebase Database reference
         feedbackReference = FirebaseDatabase.getInstance().getReference("feedback");
 
-        // Initialize views
         listViewFeedback = findViewById(R.id.listViewFeedback);
         feedbackList = new ArrayList<>();
+        feedbackAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, feedbackList);
+        listViewFeedback.setAdapter(feedbackAdapter);
+
+        // Initialize and set up the Spinner for event selection
+        Spinner spinnerEvents = findViewById(R.id.spinnerEvents);
+        ArrayAdapter<String> eventAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new ArrayList<>());
+        eventAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEvents.setAdapter(eventAdapter);
+
+        // Set up item selection listener for the Spinner
+        spinnerEvents.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Update feedback based on the selected event
+                displayFeedbackForEvent(spinnerEvents.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing here
+            }
+        });
 
         displayFeedback();
 
@@ -42,30 +66,37 @@ public class FeedbackActivity extends AppCompatActivity {
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Finish the current activity and go back to the main page
                 finish();
             }
         });
     }
 
     private void displayFeedback() {
-
-
         feedbackReference.addValueEventListener(new ValueEventListener() {
-
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // Clear the previous feedback
                 feedbackList.clear();
+                List<String> eventNames = new ArrayList<>();
+
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     String comment = snapshot.child("comment").getValue(String.class);
                     long rating = snapshot.child("rating").getValue(Long.class);
-                    String feedbackEntry = comment + " \nRating: " + rating + "/5";
+                    String eventName = snapshot.child("name").getValue(String.class);
+
+                    String feedbackEntry = "Event: " + eventName + "\nComment: " + comment + " \nRating: " + rating + "/5";
                     feedbackList.add(feedbackEntry);
+
+                    // Populate the list of event names for the Spinner
+                    if (!eventNames.contains(eventName)) {
+                        eventNames.add(eventName);
+                    }
                 }
 
-                // Update the ListView and the bottom panel
-                updateFeedbackListView();
+                // Update the Spinner with event names
+                Spinner spinnerEvents = findViewById(R.id.spinnerEvents);
+                ArrayAdapter<String> eventAdapter = new ArrayAdapter<>(FeedbackActivity.this, android.R.layout.simple_spinner_item, eventNames);
+                eventAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerEvents.setAdapter(eventAdapter);
             }
 
             @Override
@@ -73,36 +104,52 @@ public class FeedbackActivity extends AppCompatActivity {
                 // Handle errors, if any
             }
         });
+    }
 
+    private void displayFeedbackForEvent(String selectedEvent) {
+        // Filter feedbackList for the selected event
+        List<String> filteredFeedbackList = new ArrayList<>();
+        for (String feedback : feedbackList) {
+            if (feedback.contains("Event: " + selectedEvent)) {
+                filteredFeedbackList.add(feedback);
+            }
+        }
 
+        // Update the ListView and the bottom panel for the selected event
+        feedbackAdapter.clear();
+        feedbackAdapter.addAll(filteredFeedbackList);
+        feedbackAdapter.notifyDataSetChanged();
+
+        updateFeedbackListView(filteredFeedbackList);
+        updateBottomPanel(filteredFeedbackList);
 
     }
 
-    // Update the ListView with the current feedbackList
-    private void updateFeedbackListView() {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, feedbackList);
+    private void updateFeedbackListView(List<String> updatedFeedbackList) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, updatedFeedbackList);
         listViewFeedback.setAdapter(adapter);
+    }
 
+    private void updateBottomPanel(List<String> feedbackForEvent) {
         // Update the count of reviews
         TextView tvReviewCount = findViewById(R.id.tvReviewCount);
-        tvReviewCount.setText("Reviews: " + feedbackList.size());
+        tvReviewCount.setText("Reviews: " + feedbackForEvent.size());
 
         // Calculate and update the average rating
-        float averageRating = calculateAverageRating();
+        float averageRating = calculateAverageRating(feedbackForEvent);
         TextView tvAverageRating = findViewById(R.id.tvAverageRating);
         tvAverageRating.setText(String.format("Avg Rating: %.1f", averageRating));
     }
 
-    // Calculate the average rating from the current feedbackList
-    private float calculateAverageRating() {
-        if (feedbackList.isEmpty()) {
+    private float calculateAverageRating(List<String> feedbackForEvent) {
+        if (feedbackForEvent.isEmpty()) {
             return 0; // To avoid division by zero
         }
 
-        int totalReviews = feedbackList.size();
+        int totalReviews = feedbackForEvent.size();
         float totalRating = 0;
 
-        for (String feedbackEntry : feedbackList) {
+        for (String feedbackEntry : feedbackForEvent) {
             int rating = Integer.parseInt(feedbackEntry.substring(feedbackEntry.lastIndexOf(":") + 2, feedbackEntry.lastIndexOf("/")));
             totalRating += rating;
         }
