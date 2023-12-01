82bc1485adf73d2df3807b59bf906a721b080042
anish422 <anish_shah0516@hotmail.com>
2023-12-01 01:23:03 -0500

    Added user story 2 feature

--- app/src/main/java/com/example/b07_project/PostreqActivity.java
+++ app/src/main/java/com/example/b07_project/PostreqActivity.java
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class PostreqActivity extends AppCompatActivity {
 
-    private RadioGroup radioGroupLevel;
-    private RadioGroup radioGroupProgram;
+    private RadioGroup radioGroupLevel, radioGroupCredits;
     private Button btnSubmit;
-    private TextView tvRequirements;
+    private EditText editTextCourse1, editTextCourse2, editTextCourse3, editTextCourse4, editTextCourse5;
+    private String selectedLevel, selectedCredits;
+    private double gradeCourse1, gradeCourse2, gradeCourse3, gradeCourse4, gradeCourse5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,42 +23,82 @@ public class PostreqActivity extends AppCompatActivity {
         setContentView(R.layout.activity_postreq);
 
         radioGroupLevel = findViewById(R.id.radioGroupLevel);
-        radioGroupProgram = findViewById(R.id.radioGroupProgram);
+        radioGroupCredits = findViewById(R.id.radioGroupCredits);
         btnSubmit = findViewById(R.id.btnSubmit);
-        tvRequirements = findViewById(R.id.tvRequirements);
+        editTextCourse1 = findViewById(R.id.editTextCourse1);
+        editTextCourse2 = findViewById(R.id.editTextCourse2);
+        editTextCourse3 = findViewById(R.id.editTextCourse3);
+        editTextCourse4 = findViewById(R.id.editTextCourse4);
+        editTextCourse5 = findViewById(R.id.editTextCourse5);
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                saveValues();
                 showRequirements();
+            }
+        });
+
+        radioGroupLevel.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton selectedRadioButton = findViewById(checkedId);
+                selectedLevel = selectedRadioButton.getText().toString();
+            }
+        });
+
+        radioGroupCredits.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton selectedRadioButton = findViewById(checkedId);
+                selectedCredits = selectedRadioButton.getText().toString();
             }
         });
     }
 
-    private void showRequirements() {
-        String level = getSelectedRadioButtonText(radioGroupLevel);
-        String program = getSelectedRadioButtonText(radioGroupProgram);
-
-        // Determine requirements based on level and program
-        String requirements = getRequirements(level, program);
-
-        // Display requirements in the TextView
-        tvRequirements.setText(requirements);
+    private void saveValues() {
+        // Save the entered grades in variables
+        gradeCourse1 = getGradeFromEditText(editTextCourse1);
+        gradeCourse2 = getGradeFromEditText(editTextCourse2);
+        gradeCourse3 = getGradeFromEditText(editTextCourse3);
+        gradeCourse4 = getGradeFromEditText(editTextCourse4);
+        gradeCourse5 = getGradeFromEditText(editTextCourse5);
     }
 
-    private String getSelectedRadioButtonText(RadioGroup radioGroup) {
-        int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
-        if (selectedRadioButtonId != -1) {
-            RadioButton selectedRadioButton = findViewById(selectedRadioButtonId);
-            return selectedRadioButton.getText().toString();
+    private double getGradeFromEditText(EditText editText) {
+        String gradeString = editText.getText().toString();
+        if (!gradeString.isEmpty()) {
+            try {
+                return Double.parseDouble(gradeString);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
         }
-        return "";
+        return 0.0; // Default value if the grade is not entered or invalid
     }
 
-    private String getRequirements(String level, String program) {
-        // You can customize this method to provide specific requirements
-        // based on the combination of level and program selected.
-        // This is a simple example; you can replace it with your logic.
-        return "Requirements for " + level + " in " + program;
+    private void showRequirements() {
+       double avg = (gradeCourse1 + gradeCourse2 + gradeCourse3+ gradeCourse4 + gradeCourse5)/5;
+
+        // Example: Display the saved values in the TextView
+        TextView tvRequirements = findViewById(R.id.tvRequirements);
+        tvRequirements.setText(selectedLevel);
+
+//        if (selectedLevel == "Minor"){
+//            if (gradeCourse5 >= 0.7 && gradeCourse4 >= 0.7 &&gradeCourse3 >= 0.7 &&gradeCourse2 >= 0.7 &&gradeCourse1 >= 0.7 && selectedCredits=="Yes"){
+//                tvRequirements.setText("You qualify for Computer Science POSt Minor.");
+//            }
+//        }
+//
+//        else {
+//            if (gradeCourse5 >= 0.7 && gradeCourse4 >= 0.7 &&gradeCourse3 >= 0.7 &&gradeCourse2 >= 0.7 &&gradeCourse1 >= 0.7 && selectedCredits=="Yes" && avg>=2.5 && gradeCourse2>=3.0 && (gradeCourse5>2.3&&gradeCourse4>2.3 || gradeCourse5>2.3&&gradeCourse1>2.3|| gradeCourse4>2.3&&gradeCourse1>2.3)){
+//                tvRequirements.setText("You qualify for Computer Science POSt Major / Specialist.");
+//            }
+//
+//            else{
+//                tvRequirements.setText("You do not qualify for Computer Science POSt.");
+//            }
+//        }
+
     }
 }
