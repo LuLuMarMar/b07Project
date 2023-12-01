b44579aff6ecb3639e498c6bc16c187ea81b56a1
Rhodwell <rjb.malicdem@gmail.com>
2023-12-01 16:43:01 -0500

    Added all the features for User Story #5&6

--- app/src/main/java/com/example/b07_project/EventActivity.java
+++ app/src/main/java/com/example/b07_project/EventActivity.java
@@ -2,11 +2,22 @@ package com.example.b07_project;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.app.AlertDialog;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +30,11 @@ import java.util.List;
 
 public class EventActivity extends AppCompatActivity {
 
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog alertDialog;
+    private TextView eventName;
+    private TextView eventDate;
+    private TextView eventSpace;
     private DatabaseReference eventReference;
     private ListView listViewEvent;
     private List<String> eventList;
@@ -27,11 +43,7 @@ public class EventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-
-        // Initialize Firebase Database reference
         eventReference = FirebaseDatabase.getInstance().getReference("events");
-
-        // Initialize views
         listViewEvent = findViewById(R.id.listViewEvent);
         eventList = new ArrayList<>();
 
@@ -55,6 +67,41 @@ public class EventActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        listViewEvent.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                getEvent(position, new EventCallback() {
+                    @Override
+                    public void onEventRetrieved(String eventKey) {
+                        DatabaseReference eventSelectedReference =  eventReference.child(eventKey);
+                        eventSelectedReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.exists()) {
+                                    String name = dataSnapshot.child("name").getValue(String.class);
+                                    long limit = dataSnapshot.child("limit").getValue(Long.class);
+                                    long day = dataSnapshot.child("day").getValue(Long.class);
+                                    long month = dataSnapshot.child("month").getValue(Long.class);
+                                    long year = dataSnapshot.child("year").getValue(Long.class);
+                                    String date = month + "/" + day + "/" + year;
+                                    showAlertDialog(R.layout.layout_event, name, date, limit, eventKey);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                // Handle error
+                            }
+                        });
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Handle cancellation or error
+                        Log.e("FirebaseError", "Error: " + databaseError.getMessage());
+                    }
+                });
+            }
+        });
     }
 
     private void displayFeedback() {
@@ -65,16 +112,8 @@ public class EventActivity extends AppCompatActivity {
                 // Clear the previous feedback
                 eventList.clear();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    String name = snapshot.child("name").getValue(String.class);
-                    long limit = snapshot.child("limit").getValue(Long.class);
-                    long day = snapshot.child("day").getValue(Long.class);
-                    long month = snapshot.child("month").getValue(Long.class);
-                    long year = snapshot.child("year").getValue(Long.class);
-                    String feedbackEntry = "Event: " + name + "\nDate: " + month + "/" + day + "/" + year;
-                    eventList.add(feedbackEntry);
+                    eventList.add(snapshot.child("name").getValue(String.class));
                 }
-
-                // Update the ListView and the bottom panel
                 updateFeedbackListView();
             }
 
@@ -85,9 +124,89 @@ public class EventActivity extends AppCompatActivity {
         });
     }
 
-    // Update the ListView with the current feedbackList
     private void updateFeedbackListView() {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, eventList);
         listViewEvent.setAdapter(adapter);
     }
+
+    public interface EventCallback {
+        void onEventRetrieved(String eventKey);
+        void onCancelled(DatabaseError databaseError);
+    }
+
+    public void getEvent(int position, final EventCallback callback) {
+        eventReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int current = 0;
+                for (DataSnapshot eventSnapshot : dataSnapshot.getChildren()) {
+                    if (current == position) {
+                        String eventKey = eventSnapshot.getKey();
+                        callback.onEventRetrieved(eventKey);
+                        return;
+                    }
+                    current++;
+                }
+                // If the position is out of bounds, you might want to handle this case
+                callback.onCancelled(null);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                callback.onCancelled(databaseError);
+            }
+        });
+    }
+    private void showAlertDialog(int layout, String name, String date, long limit, String eventKey){
+        final boolean[] flagButton = {false};
+        dialogBuilder = new AlertDialog.Builder(this);
+        View layoutView = getLayoutInflater().inflate(layout, null);
+
+        Button btnRSVP = layoutView.findViewById(R.id.btnRSVP);
+        Button btnAddFB = layoutView.findViewById(R.id.btnAddFB);
+        if(flagButton[0]) {
+            btnRSVP.setBackgroundColor(Color.parseColor("#8F9196"));
+        } else {
+            btnRSVP.setBackgroundColor(Color.parseColor("#F2F4F7"));
+        }
+        btnAddFB.setBackgroundColor(Color.parseColor("#F2F4F7"));
+
+        eventName = layoutView.findViewById(R.id.eventName);
+        eventDate = layoutView.findViewById(R.id.eventDate);
+        eventSpace = layoutView.findViewById(R.id.eventSpace);
+        eventName.setText(name);
+        eventDate.setText(date);
+        eventSpace.setText("Remaining Seats: " + limit);
+
+        dialogBuilder.setView(layoutView);
+        alertDialog = dialogBuilder.create();
+        alertDialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        alertDialog.getWindow().addFlags(WindowManager.LayoutParams.DIM_AMOUNT_CHANGED);
+        alertDialog.show();
+
+        btnRSVP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!flagButton[0]) {
+                    eventReference.child(eventKey).child("limit").setValue(limit - 1);
+                    Toast.makeText(EventActivity.this,
+                            "Successful RSVP", Toast.LENGTH_LONG).show();
+                    btnRSVP.setEnabled(false);
+                    btnRSVP.setBackgroundColor(Color.parseColor("#8F9196"));
+                    flagButton[0] = true;
+                }
+            }
+        });
+        btnAddFB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EventActivity.this, FeedbackFragment.class);
+                intent.putExtra("eventName", name);
+                startActivity(intent);
+                alertDialog.dismiss();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/b07_project/FeedbackFragment.java
+++ app/src/main/java/com/example/b07_project/FeedbackFragment.java
@@ -1,5 +1,6 @@
 package com.example.b07_project;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,24 +17,25 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class FeedbackFragment extends AppCompatActivity {
-
     private DatabaseReference feedbackReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_feedback);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("eventName");
         feedbackReference = FirebaseDatabase.getInstance().getReference("feedback");
         Button addFeedbackToDB = findViewById(R.id.feedback_button);
         addFeedbackToDB.setBackgroundColor(Color.parseColor("#007FA3"));
         RadioGroup ratingGroup = findViewById(R.id.ratingGroup);
         TextInputEditText commentInput = findViewById(R.id.comment_text_input);
-        EditText nameInput = findViewById(R.id.eventNameText);
+        TextView fbText = findViewById(R.id.feedbackText);
+        fbText.setText("How was your experience in \n" + name);
         addFeedbackToDB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String key = feedbackReference.push().getKey();
                 String comment = commentInput.getText().toString();
-                String name = nameInput.getText().toString();
                 int rating = ratingGroup.getCheckedRadioButtonId();
                 if(rating != -1) {
                     RadioButton ratingButton = findViewById(rating);
@@ -40,16 +43,11 @@ public class FeedbackFragment extends AppCompatActivity {
                 } else {
                     rating = 0;
                 }
-                if(name.isEmpty()) {
-                    Toast.makeText(FeedbackFragment.this,
-                            "Please include the name of the event", Toast.LENGTH_SHORT).show();
-                } else {
-                    Feedback feedback = new Feedback(rating, comment, name);
-                    feedbackReference.child(key).setValue(feedback);
-                    Toast.makeText(FeedbackFragment.this,
-                            "Thank you for your feedback", Toast.LENGTH_LONG).show();
-                    finish();
-                }
+                Feedback feedback = new Feedback(rating, comment, name);
+                feedbackReference.child(key).setValue(feedback);
+                Toast.makeText(FeedbackFragment.this,
+                        "Thank you for your feedback", Toast.LENGTH_LONG).show();
+                finish();
             }
         });
     }
