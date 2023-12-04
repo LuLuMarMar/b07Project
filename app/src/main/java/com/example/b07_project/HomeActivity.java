2a297460e0ce96819d424a7bb52789e5a60f52e9
Rhodwell <rjb.malicdem@gmail.com>
2023-12-03 21:52:18 -0500

    Fixed some bugs and improved UI

--- app/src/main/java/com/example/b07_project/HomeActivity.java
+++ app/src/main/java/com/example/b07_project/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.b07_project;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,109 +21,98 @@ public class HomeActivity extends AppCompatActivity {
         String email;
 
         user = FirebaseAuth.getInstance().getCurrentUser();
-        email = user.getEmail().toString();
+        assert user != null;
+        email = user.getEmail();
+        assert email != null;
         boolean isAdmin = email.contains("@admin_mail");
 
+        //Connecting buttons from layout and changing colors
+        Button btnAddFeedback = findViewById(R.id.btnViewEvents);
+        btnAddFeedback.setBackgroundColor(Color.parseColor("#007FA3"));
+        Button btnComplaints = findViewById(R.id.btnComplaints);
+        btnComplaints.setBackgroundColor(Color.parseColor("#007FA3"));
+        Button btnPostReq = findViewById(R.id.btnPostReq);
+        btnPostReq.setBackgroundColor(Color.parseColor("#007FA3"));
+        Button btnViewAnnouncements = findViewById(R.id.btnNotifications);
+        btnViewAnnouncements.setBackgroundColor(Color.parseColor("#007FA3"));
+        Button btnViewFeedback = findViewById(R.id.btnViewFeedback);
+        btnViewFeedback.setBackgroundColor(Color.parseColor("#007FA3"));
+        Button switchComplaintsButton = findViewById(R.id.btnViewComplaints);
+        switchComplaintsButton.setBackgroundColor(Color.parseColor("#007FA3"));
+        Button switchAnnouncementsButton = findViewById(R.id.btnPostAnnouncements);
+        switchAnnouncementsButton.setBackgroundColor(Color.parseColor("#007FA3"));
+        Button btnGoToForm = findViewById(R.id.btnGoToForm);
+        btnGoToForm.setBackgroundColor(Color.parseColor("#007FA3"));
+
         if (!isAdmin) {
+            //Set visibility of admin actions to invisible
+            btnViewFeedback.setVisibility(View.GONE);
+            switchComplaintsButton.setVisibility(View.GONE);
+            switchAnnouncementsButton.setVisibility(View.GONE);
+            btnGoToForm.setVisibility(View.GONE);
+
             //View Events Button
-            Button btnAddFeedback = findViewById(R.id.btnViewEvents);
-            btnAddFeedback.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(HomeActivity.this, EventActivity.class);
-                    startActivity(intent);
-                }
+            btnAddFeedback.setOnClickListener(v -> {
+                Intent intent = new Intent(HomeActivity.this, EventActivity.class);
+                startActivity(intent);
             });
 
             //Complaints button
-            Button btnComplaints = findViewById(R.id.btnComplaints);
-            btnComplaints.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Open ComplaintActivity when the button is clicked
-                    Intent intent = new Intent(HomeActivity.this, ComplaintActivity.class);
-                    startActivity(intent);
-                }
+            btnComplaints.setOnClickListener(v -> {
+                // Open ComplaintActivity when the button is clicked
+                Intent intent = new Intent(HomeActivity.this, ComplaintActivity.class);
+                startActivity(intent);
             });
 
             //Add Post Button
-            Button btnPostReq = findViewById(R.id.btnPostReq);
-            btnPostReq.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Open FeedbackActivity when the button is clicked
-                    Intent intent = new Intent(HomeActivity.this, PostreqActivity.class);
-                    startActivity(intent);
-                }
+            btnPostReq.setOnClickListener(v -> {
+                // Open FeedbackActivity when the button is clicked
+                Intent intent = new Intent(HomeActivity.this, PostreqActivity.class);
+                startActivity(intent);
             });
 
             //Add Receive Notifications/Announcements
-            Button btnViewAnnouncements = findViewById(R.id.btnNotifications);
-            btnViewAnnouncements.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Open FeedbackActivity when the button is clicked
-                    Intent intent = new Intent(HomeActivity.this, NotificationsActivity.class);
-                    startActivity(intent);
-                }
+            btnViewAnnouncements.setOnClickListener(v -> {
+                // Open FeedbackActivity when the button is clicked
+                Intent intent = new Intent(HomeActivity.this, NotificationsActivity.class);
+                startActivity(intent);
             });
-        }
-
-        else {
+        } else {
             //View Feedback Button (For Events)
-            Button btnViewFeedback = findViewById(R.id.btnViewFeedback);
-            btnViewFeedback.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Open FeedbackActivity when the button is clicked
-                    Intent intent = new Intent(HomeActivity.this, FeedbackActivity.class);
-                    startActivity(intent);
-                }
+            btnViewFeedback.setOnClickListener(v -> {
+                // Open FeedbackActivity when the button is clicked
+                Intent intent = new Intent(HomeActivity.this, FeedbackActivity.class);
+                startActivity(intent);
             });
 
             //View Complaints Button
-            Button switchComplaintsButton = findViewById(R.id.btnViewComplaints);
-            switchComplaintsButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(HomeActivity.this, ReceiveComplaintActivity.class);
-                    startActivity(intent);
-                }
+            switchComplaintsButton.setOnClickListener(v -> {
+                Intent intent = new Intent(HomeActivity.this, ReceiveComplaintActivity.class);
+                startActivity(intent);
             });
 
             //Post Announcements Button
-            Button switchAnnouncementsButton = findViewById(R.id.btnPostAnnouncements);
-            switchAnnouncementsButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(HomeActivity.this, PostAnnouncementsActivity.class);
-                    startActivity(intent);
-                }
+            switchAnnouncementsButton.setOnClickListener(v -> {
+                Intent intent = new Intent(HomeActivity.this, PostAnnouncementsActivity.class);
+                startActivity(intent);
             });
 
             //Schedule Events Button
-            Button btnGoToForm = findViewById(R.id.btnGoToForm);
-            btnGoToForm.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Open FormActivity when the button is clicked
-                    Intent intent = new Intent(HomeActivity.this, FormActivity.class);
-                    startActivity(intent);
-                }
+            btnGoToForm.setOnClickListener(v -> {
+                // Open FormActivity when the button is clicked
+                Intent intent = new Intent(HomeActivity.this, FormActivity.class);
+                startActivity(intent);
             });
         }
 
         //Sign Out Button
         Button btnSignOut = findViewById(R.id.btnSignOut);
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Open FeedbackActivity when the button is clicked
-                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
+        btnSignOut.setBackgroundColor(Color.parseColor("#007FA3"));
+        btnSignOut.setOnClickListener(v -> {
+            //Sign out the user and go back to the login page
+            FirebaseAuth.getInstance().signOut();
+            finish();
         });
     }
-
 }
 
