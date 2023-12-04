d48ca2def998b6d0a356440122cd1d5994918369
Helina Wu <yunke.wu@mail.utoronto.ca>
2023-12-03 23:19:09 -0500

    Your commit message here

---
+++ app/src/main/java/com/example/b07_project/AnnouncementModel.java
@@ -0,0 +1,16 @@
+package com.example.b07_project.view;
+
+public class AnnouncementModel {
+    private String announcement;
+
+    public AnnouncementModel() {
+    }
+
+    public String getAnnouncement() {
+        return announcement;
+    }
+
+    public void setAnnouncement(String announcement) {
+        this.announcement = announcement;
+    }
+}
