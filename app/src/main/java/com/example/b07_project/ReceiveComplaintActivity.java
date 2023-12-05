40dc74718f612ebd9004297ffa114ec3a242fd48
Helina Wu <yunke.wu@mail.utoronto.ca>
2023-12-04 22:57:01 -0500

    Modified UI to adhere with the design style

--- app/src/main/java/com/example/b07_project/ReceiveComplaintActivity.java
+++ app/src/main/java/com/example/b07_project/ReceiveComplaintActivity.java
@@ -1,5 +1,4 @@
 package com.example.b07_project;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +21,7 @@ public class ReceiveComplaintActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_complaint);
 
-        Button backToMainButton = findViewById(R.id.backToPostButton);
+        Button backToMainButton = findViewById(R.id.backToMainButton);
         DatabaseReference complaintsReference = FirebaseDatabase.getInstance().getReference().child("complaints");
 
         ListView complaintsListView = findViewById(R.id.complaintsListView);
